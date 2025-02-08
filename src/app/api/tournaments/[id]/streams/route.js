b111c846fb23117bb54e9930ec82d5978d9763fd
Eve <gibney.eg@gmail.com>
2025-02-08 17:16:46 +0000

    Conveting streaming to use twitch api

--- src/app/api/tournaments/[id]/streams/route.js
+++ src/app/api/tournaments/[id]/streams/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
 import prisma from "../../../../../lib/prisma";
 import { authOptions } from "../../../auth/[...nextauth]/route";
-import { validateStream, getStreamInfo } from '../../../../../lib/twitch';
+import { validateStream, getStreamInfo } from "../../../../../../lib/twitch";
 
 export async function POST(request, context) {
   try {
